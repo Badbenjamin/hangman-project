62d8123ad76a5828c6e4be617d4ef4281509c28f
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-15 22:24:48 -0400

    added components, routes, and NavBar

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Game from './components/Game';
+import AddWordPage from './components/AddWordPage';
+
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/game",
+        element: <Game />
+      },
+      {
+        path: "/add_word",
+        element: <AddWordPage />
+      }
+    ]
+  }
+]
+
+const router = createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
