47fd0a92d81a315372148a03f9f74d808ccedaaf
ben mccarthy <bmccarthy90@gmail.com>
2024-08-20 11:45:50 -0400

    guesses left red or green

--- src/components/TurnsLeft.js
+++ src/components/TurnsLeft.js
@@ -2,12 +2,20 @@ import './ComponentStyles.css'
 
 function TurnsLeft({ wrongGuesses, winOrLoss}) {
 
+    let guessColor;
+
+    if (winOrLoss === true) {
+        guessColor = "guess_green"
+    } else if (winOrLoss === false) {
+        guessColor = "guess_red"
+    } else {
+        guessColor = ""
+    }
 
     return (
         <div className="turns_left">
-            <div>{`GUESSES LEFT: ${6 - wrongGuesses.length}`}</div>
+            <div>GUESSES LEFT: <span className={guessColor}>{6 - wrongGuesses.length}</span></div>
             <div id="wrong">{wrongGuesses}</div>
-            {(winOrLoss === true) ? <>WINNER</> : (winOrLoss === false) ? <>LOSER</> : <></>}
         </div>
     )
 }
