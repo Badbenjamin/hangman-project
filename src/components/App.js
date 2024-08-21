ef16b4d8a8a0650db752da0f31ea1beae6f17141
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-20 21:30:18 -0400

    got shuffle working and updated the history page

--- src/components/App.js
+++ src/components/App.js
@@ -10,59 +10,48 @@ import EditWordPage from '../pages/EditWordPage';
 
 
 function App() {
-  
+
   const [words, setWords] = useState([])
   const [currentWord, setCurrentWord] = useState("")
   const [wordIndex, setWordIndex] = useState(0)
-  const [shuffleedWords, setShuffledWords] = useState([])
-  // const shuffleWords = [...words]
   console.log(currentWord)
-
-  
-  // console.log("words", words)
-  // console.log("sw", shuffleWords)
 
   useEffect(() => {
     fetch("http://localhost:4000/words")
       .then(response => response.json())
-      .then(newWords => setWords(newWords));
+      .then(newWords => setWords(shuffleArray(newWords)));
   }, [])
 
-  useEffect(()=>{
-    function shuffleArray(array){
-      let i = array.length, j, temp;
-      while (--i > 0) {
-        j = Math.floor(Math.random() *  (i + 1));
-        temp = array[j];
-        array[j] = array[i];
-        array[i] = temp;
-      }
-      return array
+
+  function shuffleArray(array) {
+    let i = array.length, j, temp;
+    while (--i > 0) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
     }
-    // shuffleArray(...words)
-    setShuffledWords(shuffleArray([...words]))
-  }, [words])
+    return array
+  }
 
-  console.log(shuffleedWords)
-  console.log(words)
 
-  useEffect(()=>{  
+  useEffect(() => {
     if (words[0] !== undefined && words[wordIndex] !== undefined) {
       setCurrentWord(words[wordIndex].word)
-    } 
-  }, [wordIndex])
+    }
+  }, [words, wordIndex])
 
-  function handleNextWord(){
+  function handleNextWord() {
     let count = wordIndex
-    if (currentWord !== undefined && wordIndex <= words.length -2) {
-      count ++
+    if (currentWord !== undefined && wordIndex <= words.length - 2) {
+      count++
       setWordIndex(count)
-    } else  {
+    } else {
       setWordIndex(0)
     }
   }
 
-  function addNewWord(newWord){
+  function addNewWord(newWord) {
     console.log(newWord)
     fetch("http://localhost:4000/words", {
       method: "POST",
@@ -71,64 +60,64 @@ function App() {
       },
       body: JSON.stringify(newWord)
     })
-    .then(response => response.json())
-    .then(newWordData => setWords([...words, newWordData]))
+      .then(response => response.json())
+      .then(newWordData => setWords([...words, newWordData]))
   }
-  
-  function removeWord(deletedWordId){
+
+  function removeWord(deletedWordId) {
     console.log(deletedWordId)
     fetch(`http://localhost:4000/words/${deletedWordId}`, {
       method: "DELETE"
     })
-    .then(response => {
-      if(response.ok){
-        setWords((words) => words.filter(word =>{
-          return word.id !== deletedWordId;
-        }))
-      }
-    })
-    
+      .then(response => {
+        if (response.ok) {
+          setWords((words) => words.filter(word => {
+            return word.id !== deletedWordId;
+          }))
+        }
+      })
+
   }
 
-  function editWord(updatedWord, id){
+  function editWord(updatedWord, id) {
     // console.log(updatedWord)
     // console.log(id)
     fetch(`http://localhost:4000/words/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
-        "Accept" : "application/json"
+        "Accept": "application/json"
       },
       body: JSON.stringify(updatedWord)
     })
-    .then(r => r.json())
-    .then(updatedWordData => setWords(words => words.map(word => {
-      if (updatedWordData.id === word.id) {
-        return updatedWordData
-      } else {
-        return word
-      }
-    })))
+      .then(r => r.json())
+      .then(updatedWordData => setWords(words => words.map(word => {
+        if (updatedWordData.id === word.id) {
+          return updatedWordData
+        } else {
+          return word
+        }
+      })))
   }
 
   const routes = [
     {
       path: "/",
       element: ((words[0] === undefined) ? null : <GamePage hint={words[wordIndex].hint} currentWord={currentWord} handleNextWord={handleNextWord} />),
-      errorElement: <ErrorPage/>
+      errorElement: <ErrorPage />
     },
     {
       path: "/add_word",
       element: <AddWordPage removeWord={removeWord} addNewWord={addNewWord} words={words} />,
-      errorElement: <ErrorPage/>
+      errorElement: <ErrorPage />
     },
     {
       path: "/history",
-      element: <WordHistory/>
+      element: <WordHistory />
     },
     {
       path: "/words/:id",
-      element: ((words[0] === undefined) ? null : <EditWordPage  editWord={editWord}/>)
+      element: ((words[0] === undefined) ? null : <EditWordPage editWord={editWord} />)
     }
   ]
 
--- src/pages/WordHistory.js
+++ src/pages/WordHistory.js
@@ -1,26 +1,28 @@
 import Navbar from "../components/Navbar"
 
-function WordHistory({word}){
-    return(
+function WordHistory({ word }) {
+    return (
         <div>
-        <Navbar/>
-        <h1>WHAT IS HANGMAN?</h1>
-        <p>
-            The word guessing game we know as Hangman was first mentioned in 1894 in Alice Gomme's book, <a href="https://www.gutenberg.org/files/41727/41727-h/41727-h.htm#GameI_50">Traditional Games of England, Scotland, and Ireland</a>.
-            Then, the game was called "Birds, Beasts, and Fishes." One player would choose a type of bird, beast, or fish, and write only he first and last letters of its name, with all of its inner characters marked with Xs.
-            The players would take turns guessing and choosing the word. The guesser accumulates points by guessing the entire word. They then recieve points for each blank character they correctly guessed. 
-            The word chooser gains points if the guesser cannot guess the word. They recieve a point for every blank character in the word they chose.
-        </p>
-        <p>
-            The variation that incorporated the hanging man as a way to keep score first appeared in a 1902 article in the Philidelphia Inquirer.
-            The game was popular at "White Cap" parties, which were thrown by turn-of-the-century <a href="https://en.wikipedia.org/wiki/Whitecapping">vigilante groups</a>.
-            These groups, which became popular after the Civil War and included the KKK, were responsible for adding the morbid lynching imagegry to this otherwise innocent childrens game.
-        </p>
-        <p>
-            Now that we know the history of Hangman, we can omit its namesake and most violent element, and return to a version more similar to the origional game.
+            <Navbar />
+            <h1>WHAT IS HANGMAN?</h1>
+            <p>
+                The word guessing game Hangman evolved from an English childrens game, and its earliest mention is in folklorist Alice Gomme's 1894 book, <a href="https://www.gutenberg.org/files/41727/41727-h/41727-h.htm#GameI_50">Traditional Games of England, Scotland, and Ireland</a>.
+                The game was origionally called "Birds, Beasts, and Fishes." One player would choose a type of bird, beast, or fish, and write the first and last letters of its name, with all of its inner characters marked with Xs.
+                <p>
+                </p>
+                The players would take turns guessing and choosing the word. The guesser accumulates points by guessing the entire word. They then recieve points for each blank character in the word.
+                The word chooser gains points if the guesser cannot guess the word. They recieve a point for every blank character in the word they chose.
+            </p>
+            <p>
+                The variation that incorporated the hanging man as a way to keep score first appeared in a 1902 article in the Philidelphia Inquirer.
+                The game was popular at "White Cap" parties, which were thrown by turn-of-the-century <a href="https://en.wikipedia.org/wiki/Whitecapping">vigilante groups</a>.
+                These groups, which became popular after the Civil War, and included the KKK, were responsible for adding the morbid lynching imagegry to this otherwise innocent childrens game.
+            </p>
+            <p>
+                Now that we know the history of Hangman, we can omit its namesake and most violent element, and return to a version more similar to the origional game.
 
-        </p>
-    </div>
+            </p>
+        </div>
     )
 }
 
