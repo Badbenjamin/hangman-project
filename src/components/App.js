46999e43ae9d7ca58328f6b7c5ee77afc6f5cafd
ben mccarthy <bmccarthy90@gmail.com>
2024-11-07 16:03:35 -0500

    changed url in routes"

--- src/components/App.js
+++ src/components/App.js
@@ -96,7 +96,7 @@ function App() {
 
   const routes = [
     {
-      path: "/",
+      path: "/hangman_project",
       element: <GamePage gameOver={gameOver} hint={currentHint} currentWord={currentWord} difficulty={difficulty} handleDifficultyChange={handleDifficultyChange} handleNextWord={handleNextWord} />,
       errorElement: <ErrorPage />
     },
