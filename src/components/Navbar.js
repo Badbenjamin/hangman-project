c673c52cee788a14faea271261ddea27f02887a7
ben mccarthy <bmccarthy90@gmail.com>
2024-11-07 16:11:51 -0500

    changed navlink link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './ComponentStyles.css'
 function Navbar() {
     return (
         <div className="nav_bar">
-            <NavLink className="selected" to="/">PLAY : </NavLink>
+            <NavLink className="selected" to="/hangman-project">PLAY : </NavLink>
             <NavLink className="selected" to="/add_word">ADD WORD : </NavLink>
             <NavLink className="selected" to="/history">ABOUT</NavLink>
         </div>
